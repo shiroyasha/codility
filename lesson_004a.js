ccbb84ecb5b55abf69ac337a41d38216e6b7c1ec
Igor Šarčević <igor@renderedtext.com>
2016-03-25 22:45:58 +0100

    Solution for lesson 4a

---
+++ lesson_004a.js
@@ -0,0 +1,25 @@
+// https://codility.com/demo/results/trainingSE34AE-KAK/
+
+function solution(X, A) {
+    var positions = secondsToFillPosition(A, X);
+
+    if(hasUnfilledPosition(positions)) return -1;
+
+    return Math.max.apply(null, positions);
+}
+
+function hasUnfilledPosition(positions) {
+    return positions.find(function(el) { return el === -1 });
+}
+
+function secondsToFillPosition(A, numberOfPositions) {
+    var result = Array(numberOfPositions + 1).fill(-1);
+
+    A.forEach(function(position, seconds) {
+        if(position > numberOfPositions) return;
+        if(result[position] === -1) result[position] = seconds;
+        if(result[position] > seconds) result[position] = seconds;
+    });
+
+    return result.slice(1);
+}
